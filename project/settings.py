2a81b5f78d157eca5b3e296ea194d9089a64e4d0
Glen Phelps <gphelps@verisys.com>
2014-08-08 08:16:47 -0600

    Added a dummy project for auto-generating docs and running tests.

---
+++ project/settings.py
@@ -0,0 +1,25 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+DEBUG = True
+
+SECRET_KEY = 'Not really a secret key, here for docs and testing.'
+
+INSTALLED_APPS = ('django.contrib.auth', 'django.contrib.contenttypes', 'rules')
+
+SPHINX = {'exclude_patterns': ('setup.py',)}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+try:
+    import django_nose
+except ImportError:
+    pass
+else:
+    TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+    NOSE_ARGS = ('rules', '--with-coverage', '--cover-erase', '--cover-package=rules')
+    INSTALLED_APPS += ('django_nose',)
